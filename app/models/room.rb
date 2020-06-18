2d1111dba4ca97ba4de8e5df5fcfb3216461029b
ryoken nakayama <ryoken7090@gmail.com>
2020-06-18 19:18:45 +0900

    日本語辞書導入、バリデーション追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,9 @@
 class Room < ApplicationRecord
-  validates :name, presence: true
+  with_options presence: true do
+    validates :name
+    validates :rent
+    validates :address
+  end
   has_many :stations
   accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
 end
